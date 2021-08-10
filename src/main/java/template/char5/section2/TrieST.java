fe7581247cdd84e0b6ed6c0ca014ac2e089af82b
zzyssg <zy1704528@buaa.edu.cn>
2021-08-11 00:01:08 +0800

    5.2.2 单词查找树

--- src/main/java/template/char5/section2/TrieST.java
+++ src/main/java/template/char5/section2/TrieST.java
@@ -43,10 +43,10 @@ public class TrieST<Value> {
     }
 
     public void put(String key, Value value) {
-        root = put(root, key, value,0);
+        root = put(root, key, value, 0);
     }
 
-    private Node put(Node x, String key, Value value,int d) {
+    private Node put(Node x, String key, Value value, int d) {
         //如果key存在于以x为根节点的单词查找树中则更新与之关联的值
         //出口 ： key的字符用完了即d == key.length
         if (x == null) {
@@ -70,21 +70,103 @@ public class TrieST<Value> {
 
     private Iterable<String> keysWithPrefix(String pre) {
         LinkedList<String> q = new LinkedList<>();
-        collection(get(root, pre, 0),pre,q);
+        collect(get(root, pre, 0), pre, q);
         return q;
     }
 
-    private void collection(Node x, String pre, LinkedList<String> q) {
+    private void collect(Node x, String pre, LinkedList<String> q) {
         if (x == null) {
-            return ;
+            return;
         }
         if (x.val != null) {
             q.add(pre);
         }
         for (char c = 0; c < R; c++) {
-            collection(x.next[c], pre + c, q);
+            collect(x.next[c], pre + c, q);
         }
 
+    }
+
+    public Iterable<String> keysThatMath(String pat) {
+        LinkedList<String> q = new LinkedList<>();
+        collect(root, "", pat, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, String pat, LinkedList<String> q) {
+        //本节点以前的字符组成的字符串
+        int d = pre.length();
+        //出口1 —— 未匹配到
+        if (x == null) {
+            return;
+        }
+        //匹配到时的处理
+        if (d == pat.length() && x.val != null) {
+            q.add(pre);
+        }
+        //出口2
+        if (d == pat.length()) {
+            return;
+        }
+        char ch = pat.charAt(d);
+        //递归
+        for (int c = 0; c < R; c++) {
+            if (ch == '.' || ch == c) {
+                collect(x.next[c], pre + c, pat, q);
+            }
+        }
+    }
+
+    public String longestPrefix(String s) {
+        int length = search(root, s, 0, 0);
+        return s.substring(0, length);
+    }
+
+    //到达x节点时,相关的字符串长度d
+    private int search(Node x, String s, int d, int length) {
+        //出口1 —— 到达一个空连接：单词树里没有字符与之对应
+        if (x == null) {
+            return length;
+        }
+        //有两种可能 —— 有值或者无值
+        if (x.val != null) {
+            length = d;
+        }
+        //出口2 —— s的长度 == d，可以停止了
+        if (d == s.length()) {
+            return length;
+        }
+        char c = s.charAt(d);
+        return search(x.next[c], s, d + 1, length);
+    }
+
+    public void deleteKey(String s) {
+        root = deleteKey(root, s, 0);
+    }
+
+    private Node deleteKey(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            x.val = null;
+            //TODO
+        } else {
+            //继续寻找下一个节点
+            char c = key.charAt(d);
+            x.next[c] = deleteKey(x.next[c], key, d + 1);
+        }
+        //删除后，检验
+        //如果删除后，此Node点的val和next[]的所有元素均为空，则可以删除此点 —— 正难则反
+        if (x.val != null) {
+            return x;
+        }
+        for (int c = 0; c < R; c++) {
+            if (x.next[c] != null) {
+                return x;
+            }
+        }
+        return null;
     }
 
 
